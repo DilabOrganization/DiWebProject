69dc8f00f929ab575e5a424b934d0cf469e3dc8a
Kimmoonsu <visitant2@naver.com>
2017-11-08 18:39:58 +0900

    first commit from ms

--- src/main/java/skuniv/ac/di/HomeController.java
+++ src/main/java/skuniv/ac/di/HomeController.java
@@ -27,33 +27,10 @@ import skuniv.ac.di.vo.CardVO;
  */
 @Controller
 public class HomeController {
-	String[] title= {"하둡 설치법","jdk설치법","파이썬 설치법","js 문법기초","텐서플로우 설치","삼바 설치법",
-			"스프링 설치법","css 중급","데이버베이스 연결","네트워크 프로그래밍","인서버 프로그래밍","알고리즘"};
-	String[] name= {"김건영","남기원","이규영","김주현","배준우","김문수","최준영","조현우","김연정","이재준","김윤상","이영준"};
-	String[] date= {"2017-10-01","2017-10-02","2017-10-03","2017-10-04","2017-10-05","2017-10-06","2017-10-07"
-			,"2017-10-08","2017-10-09","2017-10-10","2017-10-11","2017-10-12"};
-	int[] file_to_have= {1,2,1,1,1,1,1,2,2,2,1,1};
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/a", method = RequestMethod.GET)
-	public String home(HttpServletRequest request,HttpServletResponse response,HttpSession session) throws UnsupportedEncodingException {
+//	
+	@RequestMapping(value = "/edit", method = RequestMethod.GET)
+	public String edit(HttpServletRequest request) throws Exception {
 		
-		List<CardVO> card_list=new ArrayList<CardVO>();
-		
-		for(int i=0;i<title.length;i++) {
-			CardVO cardVO=new CardVO();
-			cardVO.setTitle(title[i]);
-			cardVO.setWriter(name[i]);
-			cardVO.setDate(date[i]);
-			cardVO.setFile_to_have(file_to_have[i]);
-			card_list.add(cardVO);
-		}
-		request.setAttribute("card_list", card_list);
-		
-		return "CardFormat";
+		return "editor";
 	}
-	
 }
